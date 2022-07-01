6b360b43ba4544fbc1147d40c91b3862edd48228
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2022-07-01 18:44:00 +0300

    Create sequoia-moonlight.min.css

---
+++ sequoia-moonlight.min.css
@@ -0,0 +1,263 @@
+re code.hljs {
+    display: block;
+    overflow-x: auto;
+    padding: 1em
+}
+
+code.hljs {
+    padding: 3px 5px
+}
+
+.hljs {
+    background: #131317
+}
+
+.hljs,
+.hljs-subst {
+    color: #868690
+}
+
+.hljs-selector-tag {
+    color: #8eb6f5
+}
+
+.hljs-selector-id {
+    color: #9898a6;
+    font-weight: 700
+}
+
+.hljs-selector-attr,
+.hljs-selector-class {
+    color: #9898a6
+}
+
+.hljs-property,
+.hljs-selector-pseudo {
+    color: #ffbb88
+}
+
+.hljs-addition {
+    background-color: rgba(142, 182, 245, 0.5)
+}
+
+.hljs-deletion {
+    background-color: rgba(245, 142, 224, 0.5)
+}
+
+.hljs-built_in,
+.hljs-class,
+.hljs-type {
+    color: #9898a6
+}
+
+.hljs-function,
+.hljs-function>.hljs-title,
+.hljs-title.hljs-function {
+    color: #ffbb88
+}
+
+.hljs-keyword,
+.hljs-literal,
+.hljs-symbol {
+    color: #8eb6f5
+}
+
+.hljs-number {
+    color: #f6c177
+}
+
+.hljs-regexp {
+    color: #f6c177
+}
+
+.hljs-string {
+    color: #8eb6f5
+}
+
+.hljs-title {
+    color: #9898a6
+}
+
+.hljs-params {
+    color: #868690
+}
+
+.hljs-bullet {
+    color: #8eb6f5
+}
+
+.hljs-code {
+    color: #9898a6
+}
+
+.hljs-emphasis {
+    font-style: italic
+}
+
+.hljs-formula {
+    color: #9898a6
+}
+
+.hljs-strong {
+    font-weight: 700
+}
+
+.hljs-link:hover {
+    text-decoration: underline
+}
+
+.hljs-comment,
+.hljs-quote {
+    color: #43444D
+}
+
+.hljs-doctag {
+    color: #9898a6
+}
+
+.hljs-meta,
+.hljs-meta .hljs-keyword {
+    color: #f58ee0
+}
+
+.hljs-meta .hljs-string {
+    color: #8eb6f5
+}
+
+.hljs-attr {
+    color: #9898a6
+}
+
+.hljs-attribute {
+    color: #868690
+}
+
+.hljs-name {
+    color: #8eb6f5
+}
+
+.hljs-section {
+    color: #ffbb88
+}
+
+.hljs-tag {
+    color: #8eb6f5
+}
+
+.hljs-template-variable,
+.hljs-variable {
+    color: #868690
+}
+
+.hljs-template-tag {
+    color: #f58ee0
+}
+
+.language-abnf .hljs-attribute {
+    color: #ffbb88
+}
+
+.language-abnf .hljs-symbol {
+    color: #f6c177
+}
+
+.language-apache .hljs-attribute {
+    color: #ffbb88
+}
+
+.language-apache .hljs-section {
+    color: #8eb6f5
+}
+
+.language-arduino .hljs-built_in {
+    color: #ffbb88
+}
+
+.language-aspectj .hljs-meta {
+    color: #fdfdfe
+}
+
+.language-aspectj>.hljs-title {
+    color: #ffbb88
+}
+
+.language-bnf .hljs-attribute {
+    color: #9898a6
+}
+
+.language-clojure .hljs-name {
+    color: #ffbb88
+}
+
+.language-clojure .hljs-symbol {
+    color: #f6c177
+}
+
+.language-coq .hljs-built_in {
+    color: #ffbb88
+}
+
+.language-cpp .hljs-meta .hljs-string {
+    color: #9898a6
+}
+
+.language-css .hljs-built_in {
+    color: #ffbb88
+}
+
+.language-css .hljs-keyword {
+    color: #fdfdfe
+}
+
+.language-diff .hljs-meta,
+.language-ebnf .hljs-attribute {
+    color: #9898a6
+}
+
+.language-glsl .hljs-built_in {
+    color: #ffbb88
+}
+
+.language-groovy .hljs-meta:not(:first-child),
+.language-haxe .hljs-meta,
+.language-java .hljs-meta {
+    color: #fdfdfe
+}
+
+.language-ldif .hljs-attribute {
+    color: #9898a6
+}
+
+.language-lisp .hljs-name,
+.language-lua .hljs-built_in,
+.language-moonscript .hljs-built_in,
+.language-nginx .hljs-attribute {
+    color: #ffbb88
+}
+
+.language-nginx .hljs-section {
+    color: #f58ee0
+}
+
+.language-pf .hljs-built_in,
+.language-processing .hljs-built_in {
+    color: #ffbb88
+}
+
+.language-scss .hljs-keyword,
+.language-stylus .hljs-keyword {
+    color: #8eb6f5
+}
+
+.language-swift .hljs-meta {
+    color: #fdfdfe
+}
+
+.language-vim .hljs-built_in {
+    color: #ffbb88;
+    font-style: italic
+}
+
+.language-yaml .hljs-meta {
+    color: #fdfdfe
+}
